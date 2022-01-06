cc291a364583575f8f9e67110abbc28c1c678ae4
Matthew Gibbons <matthew@southwestkoala.co.uk>
2022-01-06 00:45:39 +0000

    Manage font family in `app.css`

--- apps/website/tailwind.config.cjs
+++ apps/website/tailwind.config.cjs
@@ -1,15 +1,5 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		fontFamily: {
-			sans: ['Lato', ...defaultTheme.fontFamily.sans]
-		}
-	},
-	variants: {
-		extend: {}
-	},
 	plugins: [
 		require('@tailwindcss/aspect-ratio'),
 		require('@tailwindcss/forms'),
