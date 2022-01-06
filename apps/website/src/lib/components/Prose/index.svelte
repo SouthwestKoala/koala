3da76f2ab666bc5db54459e113335490694d5bb4
Matthew Gibbons <matthew@southwestkoala.co.uk>
2022-01-06 16:55:39 +0000

    Better componentisation

--- apps/website/src/lib/components/Prose/index.svelte
+++ apps/website/src/lib/components/Prose/index.svelte
@@ -1,5 +1,15 @@
-<article
-	class="prose prose-sm md:prose-lg lg:prose-xl xl:prose-2xl prose-slate dark:prose-invert prose-orange mx-auto text-justify"
+<script lang="ts">
+	import { Link } from '$lib/serializers/marks';
+
+	const serializers = {
+		marks: {
+			link: Link
+		}
+	};
+</script>
+
+<div
+	class="prose prose-sm md:prose-base lg:prose-lg xl:prose-xl 2xl:prose-2xl prose-slate dark:prose-invert prose-orange mx-auto text-justify"
 >
-	<slot />
-</article>
+	<slot name="blocks" {serializers} />
+</div>
