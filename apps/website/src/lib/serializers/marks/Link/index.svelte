073f240afb349cc99f79781d1b9ae186d4110040
Matthew Gibbons <matthew@southwestkoala.co.uk>
2022-01-06 00:46:56 +0000

    Render links

---
+++ apps/website/src/lib/serializers/marks/Link/index.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { MarkProps } from '@portabletext/svelte';
+
+	export let portableText: MarkProps<{
+		_key: string;
+		_type: 'link';
+		href: string;
+	}>;
+
+	const { _key, href } = portableText.mark;
+</script>
+
+<a {href} class="text-current font-normal decoration-orange-500 decoration-2 underline-offset-4 hover:text-orange-500"><slot /></a>
