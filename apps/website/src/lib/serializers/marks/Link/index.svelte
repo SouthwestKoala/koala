3b6a8b21eff4d63a9e2ed498904e0ed3e86bdcaa
Matthew Gibbons <matthew@southwestkoala.co.uk>
2022-01-06 16:56:15 +0000

    Format

--- apps/website/src/lib/serializers/marks/Link/index.svelte
+++ apps/website/src/lib/serializers/marks/Link/index.svelte
@@ -10,4 +10,8 @@
 	const { _key, href } = portableText.mark;
 </script>
 
-<a {href} class="text-current font-normal decoration-orange-500 decoration-2 underline-offset-4 hover:text-orange-500"><slot /></a>
+<a
+	{href}
+	class="text-current font-normal decoration-orange-500 decoration-2 underline-offset-4 hover:text-orange-500"
+	><slot /></a
+>
