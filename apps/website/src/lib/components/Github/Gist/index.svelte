70a21a997112bc8366db1c6aeb5a3118d0c46b1d
Matthew Gibbons <matthew@southwestkoala.co.uk>
2022-01-09 09:13:41 +0000

    Format

--- apps/website/src/lib/components/Github/Gist/index.svelte
+++ apps/website/src/lib/components/Github/Gist/index.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
-  export let file = '';
-  export let gist: string;
-  export let user: string;
+	export let file = '';
+	export let gist: string;
+	export let user: string;
 
-  const title = file ? file : 'Gist';
+	const title = file ? file : 'Gist';
 
-  const query = new URLSearchParams({ file })
-  const url = [`https://gist.github.com/${user}/${gist}.js`, query.toString()].filter((p) => p).join('?')
+	const query = new URLSearchParams({ file });
+	const url = [`https://gist.github.com/${user}/${gist}.js`, query.toString()]
+		.filter((p) => p)
+		.join('?');
 </script>
 
 <div class="not-prose w-full">
-  {@html `<script src="${url}"><${""}/script>`}
+	{@html `<script src="${url}"><${''}/script>`}
 </div>
