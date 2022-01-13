6bdc987b34fa6613393043917164893ac08c2138
Matthew Gibbons <matthew@southwestkoala.co.uk>
2022-01-13 05:58:20 +0000

    Config

--- packages/ui/svelte.config.js
+++ packages/ui/svelte.config.js
@@ -3,12 +3,14 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [preprocess({ postcss: true })],
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
 
 	kit: {
 		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
 };
