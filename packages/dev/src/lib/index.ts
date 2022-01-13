a3259947e82c91d2ebc9a7efa470e524ea654dca
Matthew Gibbons <matthew@southwestkoala.co.uk>
2022-01-13 06:00:41 +0000

    Dev is now part of ui

--- packages/dev/src/lib/index.ts
+++
@@ -1,1 +0,0 @@
-export { Breakpoint } from './elements';
--- packages/dev/src/routes/index.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { Breakpoint } from '$lib';
-</script>
-
-<main class="max-w-xl mx-auto space-y-8">
-	<h1 class="text-5xl tracking-tight font-black text-center">Breakpoint</h1>
-
-	<Breakpoint />
-</main>
