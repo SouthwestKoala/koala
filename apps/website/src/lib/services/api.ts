13b604a4da52c856bc28d8157d95b5b948e404ed
Matthew Gibbons <matthew@southwestkoala.co.uk>
2022-01-18 10:31:42 +0000

    Added query option

--- apps/website/src/lib/services/api.ts
+++ apps/website/src/lib/services/api.ts
@@ -2,10 +2,12 @@ import { API_BASE_URL } from '$lib/env';
 
 export const api_url = ({
 	path,
+	query,
 	base_url = API_BASE_URL,
 	version = 'v1.0'
 }: {
 	path: string;
+	query?: URLSearchParams;
 	base_url?: string;
 	version?: string;
-}): string => [base_url, version, path].join('/');
+}): string => [[base_url, version, path].join('/'), query?.toString()].filter((p) => p).join('?');
