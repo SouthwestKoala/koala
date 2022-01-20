4f244e47fcaf5b0bcf2070b3c9a035946e80b57e
Matthew Gibbons <matthew@southwestkoala.co.uk>
2022-01-20 04:36:17 +0000

    Events

--- apps/website/src/hooks.ts
+++ apps/website/src/hooks.ts
@@ -1,15 +1,17 @@
 import { dev } from '$app/env';
 import type { Handle } from '@sveltejs/kit';
 
-export const handle: Handle = async ({ request, resolve }) => {
+export const handle: Handle = async ({ event, resolve }) => {
+	const { request: { method }, url: { pathname } } = event
+
 	if (dev) {
-		console.time(`${request.method} ${request.url.pathname}`);
+		console.time(`${method} ${pathname}`);
 	}
 
-	const response = await resolve(request);
+	const response = await resolve(event);
 
 	if (dev) {
-		console.timeEnd(`${request.method} ${request.url.pathname}`);
+		console.timeEnd(`${method} ${pathname}`);
 	}
 
 	return response;
