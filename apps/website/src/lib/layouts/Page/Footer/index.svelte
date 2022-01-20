337b2021e988b9f6508b807ca3af8e41d67177b5
Matthew Gibbons <matthew@southwestkoala.co.uk>
2022-01-20 04:51:40 +0000

    Default footer

--- apps/website/src/lib/layouts/Page/Footer/index.svelte
+++ apps/website/src/lib/layouts/Page/Footer/index.svelte
@@ -1,10 +1,1 @@
-<script lang="ts">
-	import { Subscribe } from '$lib/forms';
-</script>
-
-<div class="bg-neutral-100 dark:bg-neutral-900">
-	<Subscribe />
-</div>
-
-<style lang="postcss">
-</style>
+<footer />
--- apps/website/src/lib/layouts/Page/index.svelte
+++ apps/website/src/lib/layouts/Page/index.svelte
@@ -1,13 +1,19 @@
+<script lang="ts">
+	import { Footer } from '$lib/layouts/Page';
+</script>
+
 <main class="flex flex-col gap-y-16 md:gap-y-32">
 	<slot name="header" />
 	<slot name="body">
 		<div class="flex flex-col xl:flex-row mx-auto gap-y-8 md:gap-y-16 lg:gap-x-8">
 			<slot name="left" />
-			<slot name="content" />
+			<slot />
 			<slot name="right" />
 		</div>
 	</slot>
-	<slot name="footer" />
+	<slot name="footer">
+		<Footer />
+	</slot>
 </main>
 
 <style lang="postcss">
