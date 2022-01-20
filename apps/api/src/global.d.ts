1b609f3de386108ee34b7418bf6205b8e1dc762a
Matthew Gibbons <matthew@southwestkoala.co.uk>
2022-01-20 04:35:50 +0000

    Removed redundant type

--- apps/api/src/global.d.ts
+++ apps/api/src/global.d.ts
@@ -1,12 +1,1 @@
 /// <reference types="@sveltejs/kit" />
-
-export type User = SanityDocument<{
-	_type: string;
-	_id: string;
-	sub: string;
-	likes?: {
-		_type: 'reference';
-		_key: string;
-		_ref: string;
-	}[];
-}>;
