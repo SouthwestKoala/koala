f1b180f5d86831809c58b7042ad9fcbf0f0ecf7d
Matthew Gibbons <matthew@southwestkoala.co.uk>
2022-01-20 04:37:15 +0000

    Copyright

--- apps/website/src/lib/layouts/Site/Footer/index.svelte
+++ apps/website/src/lib/layouts/Site/Footer/index.svelte
@@ -12,7 +12,7 @@
 	</div>
 	<div class="mt-8 md:mt-0 md:order-1">
 		<p class="text-center text-sm text-neutral-500">
-			&copy; {new Date().getFullYear()} Southwest Koala
+			Copyright &copy; {new Date().getFullYear()} Southwest Koala. All rights reserved.
 		</p>
 	</div>
 </footer>
