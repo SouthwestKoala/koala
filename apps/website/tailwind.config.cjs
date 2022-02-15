d7636ac58beb6ec405f90fceee73949131bbfb96
Matthew Gibbons <matthew@southwestkoala.co.uk>
2022-02-15 16:45:30 +0000

    Tweaked colour settings

--- apps/website/tailwind.config.cjs
+++ apps/website/tailwind.config.cjs
@@ -1,17 +1,5 @@
-const colors = require('tailwindcss/colors');
-
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		colors: {
-			transparent: 'transparent',
-			current: 'currentColor',
-			black: colors.black,
-			white: colors.white,
-			neutral: colors.slate,
-			accent: colors.orange
-		}
-	},
 	plugins: [
 		require('@tailwindcss/aspect-ratio'),
 		require('@tailwindcss/forms'),
